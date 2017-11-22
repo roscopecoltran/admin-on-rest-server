b6071340bf0bd92a07afe12f391675889ac26c4a
Rosco Pecoltran <rosco_pecoltran@msn.com>
2017-11-22 20:44:42 +0100

    rename main executable to

--- server/cmd/aors-gin/service.go
+++ server/cmd/aors-gin/service.go
@@ -35,49 +35,66 @@ func (s *AdminOnRestServer) Info(ctx *gin.Context) *api.Response {
 
 func (s *AdminOnRestServer) AddDataSourceUsingPOST(ctx *gin.Context, params *data_source_controller.AddDataSourceUsingPOSTParams) *api.Response {
 	pp.Println("params: ", params)
-	db := ctx.MustGet("db").(*gorm.DB)
-	var user models.User
-	var queryRes models.User
-	ctx.Bind(&user)
-	db.Where("email = ?", user.Email).First(&queryRes)
 	response := make(map[string]string)
-	if queryRes.Email != "" {
-		response["error"] = "Duplicate resource."
+	var dataSource models.DataSource
+	var queryRes models.DataSource
+	db := ctx.MustGet("db").(*gorm.DB)
+	db.Where("cluster_name = ? && db_name = ?", params.DataSource.ClusterName, params.DataSource.DbName).First(&queryRes)
+	if queryRes.DbName != "" {
+		response["status"] = "error"
+		response["msg"] = "Duplicate resource."
 	} else {
-		db.Create(&user)
-		response := make(map[string]string)
-		response["error"] = "Duplicate resource."
+		db.Create(&dataSource)
+		response["status"] = "success"
 	}
 	return &api.Response{Code: http.StatusOK, Body: response}
 }
 
 func (s *AdminOnRestServer) AddEntityUsingPOST(ctx *gin.Context, params *schema_controller.AddEntityUsingPOSTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) AddFieldUsingPOST(ctx *gin.Context, params *schema_controller.AddFieldUsingPOSTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) AddPermissionUsingPOST(ctx *gin.Context, params *permission_controller.AddPermissionUsingPOSTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) AddRoleUsingPOST(ctx *gin.Context, params *role_controller.AddRoleUsingPOSTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) AddUserUsingPOST(ctx *gin.Context, params *user_controller.AddUserUsingPOSTParams) *api.Response {
 	pp.Println("params: ", params)
+	response := make(map[string]string)
 	db := ctx.MustGet("db").(*gorm.DB)
 	var queryRes models.User
 	db.Where("email = ?", params.User.Email).First(&queryRes)
-	response := make(map[string]string)
 	if queryRes.Email != "" {
-		response["error"] = "Duplicate resource."
+		response["msg"] = "Duplicate resource."
+		response["status"] = "error"
 	} else {
 		db.Create(&params.User)
-		response["msg"] = "Success, new user created."
+		response["msg"] = "New user created."
+		response["status"] = "success"
 	}
 	return &api.Response{Code: http.StatusOK, Body: response}
 }
@@ -89,95 +106,180 @@ func (s *AdminOnRestServer) ApplyUsingPOST(ctx *gin.Context, params *apply_contr
 	db.Where("email = ?", params.Apply.Email).First(&queryRes)
 	response := make(map[string]string)
 	if queryRes.Email != "" {
-		response["error"] = "Duplicate resource."
+		response["status"] = "error"
+		response["msg"] = "Duplicate resource."
 	} else {
 		db.Create(&params.Apply)
 		response["msg"] = "Success, new user created."
+		response["status"] = "success"
 	}
 	return &api.Response{Code: http.StatusOK, Body: response}
 }
 
 func (s *AdminOnRestServer) CreateAuthenticationTokenUsingPOST(ctx *gin.Context, params *authentication_rest_controller.CreateAuthenticationTokenUsingPOSTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) DataMutationUsingDELETE(ctx *gin.Context, params *data_controller.DataMutationUsingDELETEParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) DataMutationUsingPOST(ctx *gin.Context, params *data_controller.DataMutationUsingPOSTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) DataMutationUsingPUT(ctx *gin.Context, params *data_controller.DataMutationUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) DataQueryUsingGET(ctx *gin.Context, params *data_controller.DataQueryUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) EditDataSourceUsingPUT(ctx *gin.Context, params *data_source_controller.EditDataSourceUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) EditEntityUsingPUT(ctx *gin.Context, params *schema_controller.EditEntityUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) EditFieldUsingPUT(ctx *gin.Context, params *schema_controller.EditFieldUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) EditFieldUsingPUT1(ctx *gin.Context, params *user_controller.EditFieldUsingPUT1Params) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) EditPermissionUsingPUT(ctx *gin.Context, params *permission_controller.EditPermissionUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) EditRoleUsingPUT(ctx *gin.Context, params *role_controller.EditRoleUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindAllFieldsUsingGET(ctx *gin.Context, params *schema_controller.FindAllFieldsUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindEntityFieldsUsingGET(ctx *gin.Context, params *schema_controller.FindEntityFieldsUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindOneFieldUsingGET(ctx *gin.Context, params *schema_controller.FindOneFieldUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindOneUsingGET(ctx *gin.Context, params *data_controller.FindOneUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindOneUsingGET1(ctx *gin.Context, params *schema_controller.FindOneUsingGET1Params) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindRoleUsingGET(ctx *gin.Context, params *data_source_controller.FindRoleUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindRoleUsingGET1(ctx *gin.Context, params *role_controller.FindRoleUsingGET1Params) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindUserUsingGET(ctx *gin.Context, params *permission_controller.FindUserUsingGETParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) FindUserUsingGET1(ctx *gin.Context, params *user_controller.FindUserUsingGET1Params) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) GetAuthenticatedUserUsingGET(ctx *gin.Context) *api.Response {
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
@@ -195,14 +297,24 @@ func (s *AdminOnRestServer) GetSchemasUsingGET(ctx *gin.Context) *api.Response {
 }
 
 func (s *AdminOnRestServer) ListUsingGET(ctx *gin.Context) *api.Response {
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) ListUsingGET1(ctx *gin.Context, params *permission_controller.ListUsingGET1Params) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) ListUsingGET2(ctx *gin.Context) *api.Response {
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
@@ -220,13 +332,24 @@ func (s *AdminOnRestServer) ListUsingGET3(ctx *gin.Context) *api.Response {
 }
 
 func (s *AdminOnRestServer) RefreshAndGetAuthenticationTokenUsingGET(ctx *gin.Context) *api.Response {
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) ResetCurrentDsUsingPUT(ctx *gin.Context, params *schema_controller.ResetCurrentDsUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
 
 func (s *AdminOnRestServer) SyncSchemasUsingPUT(ctx *gin.Context, params *schema_controller.SyncSchemasUsingPUTParams) *api.Response {
+	pp.Println("params: ", params)
+	// response := make(map[string]interface{})
+	// db := ctx.MustGet("db").(*gorm.DB)
+	// return &api.Response{Code: http.StatusOK, Body: response}
 	return &api.Response{Code: http.StatusNotImplemented, Body: "Not Implemented"}
 }
